e32073921c430ea4be1f3afa44764774343f2d64
zhumao <46477115+zhumao520@users.noreply.github.com>
2025-05-30 17:53:40 +0800

    1

--- webapp/core/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-核心模块初始化
-"""
-
-from .ytdlp_manager import YtdlpManager, initialize_ytdlp, get_ytdlp_manager
-from .download_manager import DownloadManager, get_download_manager
-
-__all__ = ['YtdlpManager', 'initialize_ytdlp', 'get_ytdlp_manager', 'DownloadManager', 'get_download_manager']
--- webapp/utils/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-工具模块
-"""
-
-from .url_validator import validate_url
-from .helpers import format_filesize, format_duration
-
-__all__ = ['validate_url', 'format_filesize', 'format_duration']
